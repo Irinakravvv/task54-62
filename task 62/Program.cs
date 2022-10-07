96ed789ded9ac33d9c517170a2ebe6c62394c507
unknown <tvot21@gmail.com>
2022-10-07 20:41:09 +0300

    task54

---
+++ task 62/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+
+int[,] matrix = new int[4, 4];
+
+for (int firstRowIndex = 0; firstRowIndex < 4; firstRowIndex++)
+{
+    Console.WriteLine($"Введите значение столбца {firstRowIndex + 1} первой строки");
+    matrix[0, firstRowIndex] = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int lastColumnIndex = 1; lastColumnIndex < 4; lastColumnIndex++)
+{
+    Console.WriteLine($"Введите значение строки {lastColumnIndex + 1} последнего столбца");
+    matrix[lastColumnIndex, 3] = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int lastRowIndex = 2; lastRowIndex >= 0; lastRowIndex--)
+{
+    Console.WriteLine($"Введите значение столбца {lastRowIndex + 1} последней строки");
+    matrix[3, lastRowIndex] = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int firstColumnIndex = 2; firstColumnIndex > 0; firstColumnIndex--)
+{
+    Console.WriteLine($"Введите значение строки {firstColumnIndex + 1} первого столбца");
+    matrix[firstColumnIndex, 0] = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int secondRowIndex = 1; secondRowIndex < 3; secondRowIndex++)
+{
+    Console.WriteLine($"Введите значение столбца {secondRowIndex + 1} второй строки");
+    matrix[1, secondRowIndex] = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int thirdRowIndex = 2; thirdRowIndex > 0; thirdRowIndex--)
+{
+    Console.WriteLine($"Введите значение столбца {thirdRowIndex + 1} третьей строки");
+    matrix[2, thirdRowIndex] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("В итоге получилась матрица: ");
+//printMatrixOfIntegersToConsole(matrix);
---
+++ task54/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит 
+// по убыванию элементы каждой строки двумерного массива.
+
+
+void fillMatrixWithRandomPositiveIntegers(int[,] matrix)
+{
+    Random rnd = new Random();
+    for (int rowIndex = 0; rowIndex < matrix.GetLength(0); rowIndex++)
+    {
+        for (int columnIndex = 0; columnIndex < matrix.GetLength(1); columnIndex++)
+        {
+            matrix[rowIndex, columnIndex] = rnd.Next(0, 10);
+        }
+    }
+}
+
+void printMatrixOfIntegersToConsole(int[,] matrix)
+{
+    for (int rowIndex = 0; rowIndex < matrix.GetLength(0); rowIndex++)
+    {
+        Console.Write("\n");
+        for (int columnIndex = 0; columnIndex < matrix.GetLength(1); columnIndex++)
+        {
+            Console.Write($"  {matrix[rowIndex, columnIndex]}");
+        }
+    }
+    Console.Write("\n");
+}
+
+Console.WriteLine("Введите количество строк матрицы");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов матрицы");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m < 1 || n < 1)
+{
+    Console.WriteLine("Некорректные значения");
+}
+else
+{
+    int[,] matrix = new int[m, n];
+    fillMatrixWithRandomPositiveIntegers(matrix);
+    printMatrixOfIntegersToConsole(matrix);
+
+    // insertion sort
+    for (int rowIndex = 0; rowIndex < matrix.GetLength(0); rowIndex++)
+    {
+        int columnCount = matrix.GetLength(1);
+        if (columnCount < 2)
+        {
+            break;
+        }
+        if (matrix[rowIndex, 0] < matrix[rowIndex, 1])
+        {
+            int temp = matrix[rowIndex, 0];
+            matrix[rowIndex, 0] = matrix[rowIndex, 1];
+            matrix[rowIndex, 1] = temp;
+        }
+        for (int unsorted = 2; unsorted < columnCount; unsorted++)
+        {
+            for (int sorted = 0; sorted < unsorted; sorted++)
+            {
+                if (matrix[rowIndex, unsorted] > matrix[rowIndex, sorted])
+                {
+                    int temp = matrix[rowIndex, unsorted];
+                    matrix[rowIndex, unsorted] = matrix[rowIndex, sorted];
+                    matrix[rowIndex, sorted] = temp;
+                }
+            }
+        }
+    }
+    Console.WriteLine("Матрица отсортированных строк:");
+    printMatrixOfIntegersToConsole(matrix);
+}
